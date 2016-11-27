61fe20c34c597fac634d7d08ae49b631c8ba2ed0
Boris Yurkevich <boris.yurkevich@gmail.com>
2016-11-27 17:54:43 +0000

    Initial Commit

---
+++ Commute/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Commute
+//
+//  Created by Boris Yurkevich on 27/11/2016.
+//  Copyright © 2016 Boris Yurkevich. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
